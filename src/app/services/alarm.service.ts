656a9df7e0742b941d3194578d796ccfc83620f6
Kazimglr <kzmglr025@gmail.com>
2025-07-27 17:30:17 +0300

    commit-4

--- src/app/services/alarm.service.ts
+++ src/app/services/alarm.service.ts
@@ -14,7 +14,7 @@ export interface Alarm {
   providedIn: 'root'
 })
 export class AlarmService {
-  private apiUrl = 'http://localhost:3000/alarms';
+  private apiUrl = 'https://localhost:7091/api/machines';
 
   constructor(private http: HttpClient) {}
 
